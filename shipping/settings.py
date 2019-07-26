f0ce023db6ff2d4baa84c8e9bfbbdbe1b9924d07
M Farhan Zia <farhan71727@gmail.com>
2019-07-27 01:11:37 +0500

    'gmail'

--- shipping/settings.py
+++ shipping/settings.py
@@ -155,7 +155,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "info@boltonblock.com"
-EMAIL_HOST_PASSWORD = "yfwvxwjdlcndpsai"
+EMAIL_HOST_PASSWORD = "boltondev1PW"
 # EMAIL_HOST_USER = os.environ.get('DB_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
 
