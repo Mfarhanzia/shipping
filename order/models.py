a6de84e1cef7d1022f625ffc739c7b764bb2c7b1
M Farhan Zia <farhan71727@gmail.com>
2019-12-10 23:37:59 +0500

    11:37pm-12Dec

--- order/models.py
+++ order/models.py
@@ -138,7 +138,7 @@ class ContainerPricing(models.Model):
 
 
 class CartOrder(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    order_items = models.ForeignKey(ContainerPricing, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default='')
+    order_items = models.ForeignKey(ContainerPricing, on_delete=models.CASCADE, blank=True, null=True, default='')
     quantity = models.PositiveIntegerField("Quantity")
     ordered_on = models.DateTimeField(auto_now_add=True)
