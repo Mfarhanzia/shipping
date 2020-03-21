5dc16cd17c56a2bfda681929df78aedca7b370e3
M Farhan Zia <farhan71727@gmail.com>
2020-03-21 23:13:49 +0500

    virtual visit

--- users/urls.py
+++ users/urls.py
@@ -16,25 +16,21 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset.html',
               email_template_name = 'users/password_reset_email.html'
-         ),
-         name='password_reset'),
+         ), name='password_reset'),
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='users/password_reset_done.html'
-         ),
-         name='password_reset_done'),
+         ), name='password_reset_done'),
          
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
+         ),name='password_reset_confirm'),
 
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='users/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
+         ),name='password_reset_complete'),
          
     path('', home_view, name="home"),
     
